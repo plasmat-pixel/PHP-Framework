0dc5e0322575be29068e531d1cea14c17d16b779
artemlepesch <86205531+artemlepesch@users.noreply.github.com>
2024-01-04 12:29:44 +0300

    добавил интерфейс для класса Router

--- framework/Routing/RouteContracts/RegistrarInterface.php
+++ framework/Routing/RouteContracts/RegistrarInterface.php
@@ -4,6 +4,6 @@ namespace Artem\PhpFramework\Routing\RouteContracts;
 
 interface RegistrarInterface
 {
-    public static function get(string $uri,  array $handler);
-    public static function post(string $uri,  array $handler);
+    public static function get(string $uri,  array $handler): mixed;
+    public static function post(string $uri,  array $handler): mixed;
 }
