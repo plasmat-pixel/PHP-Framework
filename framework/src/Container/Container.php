71efd38e32ac47ca4eee36398ef6c74a1561fdcf
artemlepesch <86205531+artemlepesch@users.noreply.github.com>
2024-01-06 14:45:04 +0300

    добавил исключения для своего container

--- framework/src/Container/Container.php
+++ framework/src/Container/Container.php
@@ -2,6 +2,7 @@
 
 namespace Artem\PhpFramework\Container;
 
+use Artem\PhpFramework\Container\Exceptions\ContainerException;
 use Psr\Container\ContainerInterface;
 
 class Container implements ContainerInterface
@@ -9,6 +10,13 @@ class Container implements ContainerInterface
     private array $services = [];
     public function add(string $id, string|object $concrete = null)
     {
+        if (is_null($concrete)) {
+            if (!class_exists($id)) {
+                throw new ContainerException("Service $id not found");
+            }
+            $concrete = $id;
+        }
+
         $this->services[$id] = $concrete;
     }
 
