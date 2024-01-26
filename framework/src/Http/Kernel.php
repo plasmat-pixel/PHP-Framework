f8e70b5a7be973eac2729fadae8ce4cda4519c48
artemlepesch <86205531+artemlepesch@users.noreply.github.com>
2024-01-26 20:17:04 +0300

    изменил kernel

--- framework/src/Http/Kernel.php
+++ framework/src/Http/Kernel.php
@@ -25,7 +25,6 @@ class Kernel
     public function handle(Request $request): Response
     {
         try {
-            throw new \Exception("Some error");
             [$routeHandler, $vars] = $this->router->dispatch($request, $this->container);
             $response = call_user_func_array($routeHandler, $vars);
         } catch (\Exception $e) {
