6306bf5bd58b2899bab0b80a341849cc7f284ac8
artemlepesch <86205531+artemlepesch@users.noreply.github.com>
2024-01-26 20:38:35 +0300

    начал работу с шаблонизатором twig

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use App\Services\YoutubeService;
 use Artem\PhpFramework\Http\Response;
+use Twig\Environment;
 
 class HomeController
 {
     public function __construct(
-        private readonly YoutubeService $youtubeService
+        private readonly YoutubeService $youtubeService,
+        private readonly Environment $environment
     ) {
     }
 
     public function index(): Response
     {
+        dd($this->environment);
         $content = '<h1>Some content</h1>';
         $content .= "<a href=\"{$this->youtubeService->getChannelUrl()}\">Youtube Click</a>";
         return new Response($content);
--- config/services.php
+++ config/services.php
@@ -1,6 +1,7 @@
 <?php
 
 use League\Container\Container;
+use Twig\Loader\FilesystemLoader;
 use Artem\PhpFramework\Http\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
 use Artem\PhpFramework\Routing\Router;
@@ -8,10 +9,12 @@ use League\Container\ReflectionContainer;
 use League\Container\Argument\Literal\ArrayArgument;
 use League\Container\Argument\Literal\StringArgument;
 use Artem\PhpFramework\Routing\RouteContracts\RouterInterface;
+use Twig\Environment;
 
 $dotenv = new Dotenv();
 $dotenv->load(BASE_PATH . '/.env');
 $appEnv = $_ENV['APP_ENV'];
+$viewsPath = BASE_PATH . '/resource/view';
 
 $routes = include BASE_PATH . '/routes/web.php';
 $container = new Container();
@@ -30,5 +33,9 @@ $container->extend(RouterInterface::class)
 
 $container->add(Kernel::class)
     ->addArguments([RouterInterface::class, $container]);
+
+$container->addShared('twig-loader', FilesystemLoader::class)
+    ->addArgument(new StringArgument($viewsPath));
+$container->addShared(Environment::class)->addArgument('twig-loader');
 
 return $container;
